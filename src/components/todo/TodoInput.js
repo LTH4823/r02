ebc963ca99acac5aaa32f599745a5833748eef2a
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2022-06-17 11:38:58 +0900

    TodoList test2

--- src/components/todo/TodoInput.js
+++ src/components/todo/TodoInput.js
@@ -4,15 +4,21 @@ const TodoInput = ({addTodo}) => {
 
     const [title, setTitle] = useState('')
 
+    const sendTodo = () => {
+        if (title === ''){
+            alert("제목을 입력하세요.")
+            return
+        }
+        addTodo(title)
+        setTitle('')
+    }
+
     return (
         <div>
             <h2>Todo Input</h2>
             <div>
                 <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)}/>
-                <button onClick={()=>{
-                    addTodo(title)
-                    setTitle('')
-                }}>ADD</button>
+                <button onClick={() => sendTodo() }>ADD</button>
             </div>
             <hr/>
         </div>
